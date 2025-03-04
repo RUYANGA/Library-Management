0c87001153a08c183c35608d05458a7b048b75a8
RUYANGA <ruyangam15@gmail.com>
2025-03-04 17:33:47 +0200

    Your message here

--- index.js
+++ index.js
@@ -29,13 +29,10 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { 
-        secure: process.env.NODE_ENV === 'production',  
         secure: true,
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 hour session
-        sameSite: 'lax',
-          
-        
+        sameSite: 'lax'        
         } 
 }));
 
