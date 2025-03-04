7cc4a07cab69e6f51bd8ff2804320aec325305fd
RUYANGA <ruyangam15@gmail.com>
2025-03-04 18:47:16 +0200

    Your message here

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
     }),
     cookie: { 
         secure: true,
-        httpOnly: true,
+        httpOnly: false,
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 hour session
         sameSite: 'lax'        
         } 
